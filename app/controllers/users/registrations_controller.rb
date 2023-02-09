1df4e42542191739d5ce646dbf69070e9fe9dcf4
nicole-drml <doromal.cole@gmail.com>
2023-02-09 20:11:40 +0800

    from sql to pg, added name columns

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,14 +5,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    @user = User.new(user_params)
+    super
+    if @user.save
+      format.html { notice: "Successfully created account" }
+    else
+      format.html { status: :unprocessable_entity }
+    end
+  end
 
   # GET /resource/edit
   # def edit
@@ -59,4 +65,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
 end
