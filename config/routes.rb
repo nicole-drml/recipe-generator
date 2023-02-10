7b69be939dd81f648ef0f73268e756540526c086
nicole-drml <doromal.cole@gmail.com>
2023-02-10 11:10:28 +0800

    working sign-up and sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'register', to: 'devise/registrations#create'
   end
+
+
+  # get 'home/index'
+  root 'home#index' 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
