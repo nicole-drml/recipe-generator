956000351766cb010208dc701ca4fe1b4d29f1ed
Your Name <wrnermontala@gmail.com>
2023-02-11 11:09:43 +0800

    add dotenv file, working on get recipe api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
-    def index
-    end
+  def index
+    response = HTTParty.get('https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=2b8378b3&app_key=887a2a49455e11b8ef7f98c6cd05c159')
+    @response = JSON.parse(response.body)
+  end
 end
