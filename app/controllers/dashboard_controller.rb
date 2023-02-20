56a8b2979f28f99d309bf178c02fe298a5109ec2
nicole-drml <doromal.cole@gmail.com>
2023-02-21 02:24:47 +0800

    finished api queries

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
     before_action :authenticate_user! 
-    before_action :query_options, only: [:search]
+    before_action :query_options
 
     def index
     end
@@ -9,19 +9,31 @@ class DashboardController < ApplicationController
         @query = params[:query]
         @diet = params[:diet]
         @health = params[:health]
+        @cuisine = params[:cuisine]
+        @dish = params[:dish]
         get_api
     end
 
     def get_api
         if @query
-            @search_response = SearchService.search(@query, @diet)
+            @search_response = SearchService.search(@query, @diet, @health, @cuisine, @dish)
         end
     end
 
     private
 
     def query_options
-        @diet_array = [ '', "Balanced", "High-fiber", "Low-carb", "Low-fat", "Low-sodium"]
-        @health_array = [ '', 'alcohol-cocktail', 'alcohol-free', 'celery-free', 'chips']
+        @diet_array = [ '', "balanced", "high-fiber", "high-protein", "low-carb", "low-fat", "low-sodium"]
+        @health_array = [ '', 'alcohol-cocktail', 'alcohol-free', 'celery-free', 'crustacean-free', 'dairy-free', 
+            'DASH', 'egg-free', 'fish-free', 'foodmap-free', 'gluten-free', 'immuno-supportive', 'keto-friendly',
+            'kidney-friendly', 'kosher', 'low-fat-abs', 'low-potassium', 'low-sugar', 'lupine-free', 'meditteranean',
+            'mollusk-free', 'mustard-free', 'no-oil-added', 'paleo', 'peanut-free', 'pescatarian', 'pork-free',
+            'red-meat-free', 'sesame-free', 'shellfish-free', 'soy-free', 'sugar-conscious', 'sulfite-free', 
+            'tree-nut-free', 'vegan', 'vegetarian']
+        @cuisine_array = ['', 'american', 'asian', 'british', 'caribbean', 'central europe', 'chinese',
+            'eastern europe', 'french', 'indian', 'italian', 'japanese', 'kosher', 'mediterranean', 'mexican',
+            'middle eastern', 'nordic', 'south american', 'south east asian']   
+        @dish_array = ['', 'biscuits and cookies', 'bread', 'cereals', 'condiments and sauces', 'desserts', 'drinks', 
+            'main course', 'pancake', 'preps', 'preserve', 'salad', 'sandwiches', 'side dish', 'soup', 'starter', 'sweets']
     end
 end
--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -1,16 +1,27 @@
 class SearchService 
     @app_id = ENV['EDAMAM_APP_ID']
     @app_key = ENV['EDAMAM_APP_KEY']
-    @url = ''
-    def self.search(query, diet)
-        if diet
-            response = HTTParty.get("https://api.edamam.com/api/recipes/v2?type=public&q=#{query}&app_id=#{@app_id}&app_key=#{@app_key}&diet=#{diet}")
-        else
-            response = HTTParty.get("https://api.edamam.com/api/recipes/v2?type=public&q=#{query}&app_id=#{@app_id}&app_key=#{@app_key}")
-        end
-   
-        recipes = response['hits']
+    
 
+    def self.search(query, diet, health, cuisine, dish)
+        request_url = "https://api.edamam.com/api/recipes/v2?type=public&q=#{query}&app_id=#{@app_id}&app_key=#{@app_key}"
+        
+        if !diet.empty?  
+            request_url.concat("&diet=#{diet}")
+        end
+        if !health.empty?    
+            request_url.concat("&health=#{health}")
+        end
+        if !cuisine.empty?   
+            request_url.concat("&cuisineType=#{cuisine}")
+        end
+        if !dish.empty?   
+            dishstr = dish.gsub(/\s/, '%20')
+            request_url.concat("&dishType=#{dishstr}")
+        end
+        response = HTTParty.get(request_url)
+        recipes = response['hits']
+       
         recipes.map do |recipe|
             recipe['recipe']['label']
         end
