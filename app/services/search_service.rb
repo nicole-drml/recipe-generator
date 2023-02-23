795ad30e0c2a077326e546143635fdbe90d0b577
Your Name <wrnermontala@gmail.com>
2023-02-23 19:46:40 +0800

    feature/exclude ingredient

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -2,12 +2,13 @@ class SearchService
   @app_id = ENV['EDAMAM_APP_ID']
   @app_key = ENV['EDAMAM_APP_KEY']
 
-  def self.search(query, diet, health, cuisine, dish)
+  def self.search(query, diet, health, cuisine, dish, exclude)
     request_url = "https://api.edamam.com/api/recipes/v2?type=public&q=#{query}&app_id=#{@app_id}&app_key=#{@app_key}"
 
     request_url.concat("&diet=#{diet}") unless diet.empty?
     request_url.concat("&health=#{health}") unless health.empty?
     request_url.concat("&cuisineType=#{cuisine}") unless cuisine.empty?
+    request_url.concat("&excluded=#{exclude}") unless exclude.empty?
     unless dish.empty?
       dishstr = dish.gsub(/\s/, '%20')
       request_url.concat("&dishType=#{dishstr}")
