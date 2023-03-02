c28b78aa6554a12c1dfe1cfbfbf778cdfa9fbae4
nicole-drml <doromal.cole@gmail.com>
2023-03-02 11:15:28 +0800

    working save recipe

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,36 +1,45 @@
 class FavoritesController < ApplicationController
-  after_action :set_favorite, only: %i[ save_recipe ]
+  # after_action :set_favorite, only: %i[ save_recipe ]
+  after_action :create, only: %i[ save_recipe ]
 
   def index
     @user = current_user
     @favorites = @user.favorites
   end
 
-    def new 
-        @user = current_user
-        @favorite = Favorite.new
+  def new 
+    @favorite = Favorite.new
+  end
 
-        @recipe_name = params[:variable_name]
-        @recipe_url = params[:variable_url]
-        @recipe_ing = params[:variable_ing]
-        @recipe_image = params[:variable_image]
 
-        render json: {name: @recipe_name, url: @recipe_url, ingredients: @recipe_ing, image: @recipe_image}
-        
+  def save_recipe 
+    @recipe_name = params[:variable_name]
+    @recipe_url = params[:variable_url]
+    @recipe_ing = params[:variable_ing]
+    @recipe_image = params[:variable_image]
+
+    render json: {name: @recipe_name, url: @recipe_url, ingredients: @recipe_ing, image: @recipe_image}
+
+  end
+
+  private
+  def create 
+    @user = current_user
+    @favorite = current_user.favorites.new(name: @recipe_name, url: @recipe_url, image: @recipe_image, ingredients: @recipe_ing)
+
+    if @favorite.save
+      flash[:notice] = 'Favorite was successfully created.'
+    else
+      render :new
     end
 
-    def create 
-      @user = current_user
-      @favorite = @user.favorites.new(favorite_params)
-    end
+    binding.pry
+  end
+  # def set_favorite
+  #   @favorite = current_user.favorites.find(params[:id])
+  # end
 
-    def save_recipe 
-    end
-
-    private
- 
-
-    def favorite_params
-      params.require(:category).permit(:name, :image_url, :ingredients)
-    end
+  def favorite_params
+    params.require(:favorite).permit(:name, :url, :image, :ingredients)
+  end
 end
